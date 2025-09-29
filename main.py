a1bd4efc2a6d02d33e528c5a20c760d621681f62
epsilon403 <belghitihatim00@gmail.com>
2025-09-29 11:50:52 +0100

    showing all the data

---
+++ main.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+df = pd.read_csv('assurance-maladie-68d92978e362f464596651.csv')
+# print(df.info())
+# print(df.describe())
+# print(df.head())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def main():
+#     print("Hello from pr-dicteur-assurance-maladie!")
+
+
+# if __name__ == "__main__":
+#     main()
